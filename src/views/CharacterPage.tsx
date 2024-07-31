3f0370a3b75ef47f5b6e44a3172d7f6d10a84c01
martinezj24 <joshua.m1996@gmail.com>
2024-07-31 18:43:17 -0400

    R&M

--- src/views/CharacterPage.tsx
+++ src/views/CharacterPage.tsx
@@ -1,8 +1,8 @@
-import { useQuery } from "urql";
-import { GET_CHARACTER } from "../graphql/Queries";
-import { Link, useParams } from "react-router-dom";
-import { Alert, Button, Card, Container, Spinner } from "react-bootstrap";
-import { useState } from "react";
+import { useQuery } from '@apollo/client';
+import { GET_CHARACTER } from '../graphql/Queries';
+import { Link, useParams } from 'react-router-dom';
+import { Alert, Button, Card, Container, Spinner } from 'react-bootstrap';
+import { useState } from 'react';
 
 interface IEpisode {
     episode: string,
@@ -10,47 +10,47 @@ interface IEpisode {
 }
 
 const CharacterPage = () => {
-    const [show, setShow] = useState(true)
-    const { id } = useParams()
-    const { data, loading, error } = useQuery(GET_CHARACTER, {
-        variables: { id }
-    })
+  const [, setShow] = useState(true);
+  const { id } = useParams();
+  const { data, loading, error } = useQuery(GET_CHARACTER, {
+    variables: { id }
+  });
 
-    if (loading) {
-        return (
-            <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-        )
-    }
-    
-    console.log(data)
-
-    if (error) {
-        return (
-            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                <Alert.Heading>Oh snap! You got an error! {error.message}</Alert.Heading>
-            </Alert>
-        )
-    }
+  if (loading) {
     return (
-        <Container>
-            <Card>
-                <Card.Img variant="top" src={data.character.image}/>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
+
+  if (error) {
+    return (
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>
+          Oh snap! You got an error! {error.message}
+        </Alert.Heading>
+      </Alert>
+    );
+  }
+
+  return (
+    <Container>
+        <Card style={{ width: '18rem', margin: '10px' }}>
+                <Card.Img variant="top" src={data.character.image} />
                 <Card.Body>
                     <Card.Title>{data.character.name}</Card.Title>
                     <Link to={'/'}>
-                    <Button variant="primary">Back to ALl Characters</Button>
+                        <Button variant="primary">Back to All Characters</Button>
                     </Link>
                     <ul>
-                        {data.character.episode.map((ep) => (
+                        {data.character.episode.map((ep: IEpisode) => (
                             <li key={ep.episode}>{ep.name} - {ep.episode}</li>
                         ))}
                     </ul>
                 </Card.Body>
-            </Card>
-        </Container>
-    )
-}
-
-export default CharacterPage
+                </Card>
+    </Container>
+    );
+};
+export default CharacterPage;
